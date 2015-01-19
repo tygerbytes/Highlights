1992db79dee140ed9972d64657ccf24322375da7
tygerbytes <tygerbytes@users.noreply.github.com>
2015-01-18 21:48:25 -0500

    Add pause functionality and comments framework
    - Pausing: App starts out in a paused state. Clicking the select button toggles the paused state.
    
    - Adding Comments: When Up is clicked, the elapsed seconds are  added to the comments array and total comments is incremented.
    
    TODO:
    - Add summary screen to show total comments and estimated duration.
    - Draw tick marks on the progress bars where each comment was made.

--- src/main.c
+++ src/main.c
@@ -1,5 +1,8 @@
 #include <pebble.h>
 
+const short MAX_RUN_MINUTES = 10;
+const short MAX_COMMENTS = 30;
+
 static Window *s_main_window;
 static Layer *s_canvas_layer;
 static TextLayer *s_timer_layer;
@@ -8,13 +11,42 @@ static GFont *s_timer_font;
 static BitmapLayer *s_background_layer;
 static GBitmap *s_background_bitmap;
 
-static int s_seconds_ellapsed = 0;
+int s_seconds_ellapsed = 0;
+
+bool isPaused = true;
+
+// TODO: Replace magic number with MAX_COMMENTS
+short comments[ 30 ]; 
+short comments_total = 0;
 
 struct time {
-	int minutes;
-	int seconds;
+	short minutes;
+	short seconds;
 };
 
+
+/* -- Method declarations -- */
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed);
+
+void handle_scheduled_vibrations(struct time t);
+
+void set_paused(bool pause);
+
+void update_timer_display(struct time t);
+
+void update_progress_bars_display(Layer *layer, GContext *ctx);
+
+void click_config_provider(Window *window);
+
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context);
+
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context);
+
+void add_comment();
+
+
+/* -- Method implementations -- */
 
 void handle_scheduled_vibrations(struct time t) {
 	
@@ -37,6 +69,26 @@ void handle_scheduled_vibrations(struct time t) {
 				break;
 		}
 	}
+}
+
+bool is_paused() {
+	return isPaused;
+}
+
+void set_paused(bool pause) {
+	
+	if (pause) {
+		// Stop ticking
+		tick_timer_service_unsubscribe();
+	}
+	else {
+		// Start ticking
+		tick_timer_service_subscribe(SECOND_UNIT , tick_handler);	
+	}
+	
+	isPaused = pause;
+	
+	// TODO: Toggle paused/play icon
 }
 
 void update_timer_display(struct time t) {
@@ -80,11 +132,56 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 	t.seconds = s_seconds_ellapsed % 60;
 	t.minutes = (s_seconds_ellapsed % 3600) / 60;
 	
+	if (t.minutes >= MAX_RUN_MINUTES) {
+		// TODO: Make this optional and configurable in settings.
+		set_paused(true);
+		
+		// TODO: Show summary screen
+		//  Total comments; Speaker time, etc.
+	}
+	
 	update_timer_display(t);	
 	
 	handle_scheduled_vibrations(t);
 	
 	s_seconds_ellapsed++;
+}
+
+void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {  
+  // Toggle paused state
+	if (is_paused()) {
+		set_paused(false);
+	}
+	else {
+		set_paused(true);
+	}	
+}
+
+void add_comment() {
+	if (comments_total == 0) {
+		// First comment!
+		
+	}
+	
+	if (comments_total < MAX_COMMENTS) {
+		comments[comments_total] = s_seconds_ellapsed;
+	}
+	
+	comments_total++;
+}
+
+void up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+	add_comment();
+}
+
+void click_config_provider(Window *window) {
+ // single select click:
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
+	
+	window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+  
+  // long click config:
+  //window_long_click_subscribe(BUTTON_ID_SELECT, 700, select_long_click_handler, select_long_click_release_handler);
 }
 
 void handle_init(void) {
@@ -116,11 +213,19 @@ void handle_init(void) {
 	// Configure fonts
 	s_timer_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
 	text_layer_set_font(s_timer_layer, s_timer_font);
-		
+	
+	// Configure buttons
+	window_set_click_config_provider(s_main_window, (ClickConfigProvider) click_config_provider);
+
+	// Init comments array
+	for ( int i = 0; i < MAX_COMMENTS; i++ ) {
+      comments[ i ] = 0;
+  }
+	
   window_stack_push(s_main_window, true);
 	
-	// Register with TickTimerService
-	tick_timer_service_subscribe(SECOND_UNIT , tick_handler);
+	// Start out paused
+	set_paused(true);
 	
 	vibes_short_pulse();
 }
